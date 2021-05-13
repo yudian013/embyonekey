1a8b66844a587c437451c479ffb3688a8d3ef7d5
yudian013 <84134697+yudian013@users.noreply.github.com>
2021-05-13 21:03:06 +0800

    Update embyonekey.sh

--- embyonekey.sh
+++ embyonekey.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-curl https://raw.githubusercontent.com/5hrike/embyonekey/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
+curl https://raw.githubusercontent.com/yudian013/embyonekey/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 cd /usr/local/etc/nginx/conf.d/*-*-*-*
-wget https://raw.githubusercontent.com/5hrike/embyonekey/master/user.conf
+wget https://raw.githubusercontent.com/yudian013/embyonekey/master/user.conf
 nginx -s reload
 
